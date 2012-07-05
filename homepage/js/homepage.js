0c51bffb1dc5935313fa40ed13c97c9ac923485a
MatSam <MadManMathew@gmail.com>
2012-07-05 17:04:11 +1000

    Add Json testimonials

--- homepage/js/homepage.js
+++ homepage/js/homepage.js
@@ -1,7 +1,29 @@
 initialNavigation("#navigationDiv",".specialNavDiv","assets/cloudsBackground.jpg","navButtonLeft","navButtonRight");
-    
+ 
 function on_doc_ready()
 {   
+    $.getJSON('js/testimonials.json',
+        function(json) {
+            $(json.testimonials).each(function (i,testimonial)
+                 {
+                     var testimonialDiv = $(".testimonialDiv:first");
+                     if(i>0){
+                        testimonialDiv.clone().appendTo(testimonialDiv.parent());   
+                     }
+                     testimonialDiv.find(".testimonialQuote").html(testimonial.quote);
+                     testimonialDiv.find(".testimonialAuthor").html(testimonial.author);
+                     testimonialDiv.find(".testimonialTitle").html(testimonial.title);
+                 });
+            if(json.testimonials.length == 0)
+                $(".testimonialDiv:first").remove();
+        });
+    
+$.ajax("https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=MadManMathew", function(data)
+      {
+          alert(data);
+      }
+);
+
 }
 $(document).ready(on_doc_ready);
 
--- homepage/js/navigation.js
+++ homepage/js/navigation.js
@@ -8,13 +8,17 @@ function initialNavigation(containerSelector,specialComponentSelector,background
         <div id="couldBG" style="position:absolute;left:-200px;width:150%;height:100%;background-image:url(\''+ backgroundPath + '\')"><\/div> \
     <\/div> \
     <div style="position:absolute;top:0px;left:0px;right:0px;bottom:0px;"> \
-        <div style="margin:auto;width:1186px;"> \
-            <div style="float:left;"><button id="leftButton" onclick="clickLeft(event)" disabled="disabled" class="'+buttonLeftClass+'"><\/button> <\/div>\
-            <div style="float:right;"><button id="rightButton" onclick="clickRight(event)" class="'+buttonRightClass+'"><\/button> <\/div>\
-        <\/div>\
-        <div  style="width:966px;margin:auto;overflow:hidden;height:350px;">\
+        <div  style="width:966px;margin:auto;overflow:hidden;height:350px;top:0px;position:relative;">\
             <div id="testNav"  style="left:0px;position:relative;"><\/div>\
             <h3 id="indexText" style="right:0px;top:0px;position:relative;text-align:right;"> 1</h3>\
+        <\/div>\
+    <\/div> \
+    <div style="position:absolute;top:0px;left:0px;right:0px;bottom:0px;"> \
+        <div  style="width:966px;margin:auto;height:350px;">\
+            <div style="left:-100px;width:1186px;position:relative;height:100%;"> \
+                <div style="float:left;"><button id="leftButton" onclick="clickLeft(event)" disabled="disabled" class="'+buttonLeftClass+'"><\/button> <\/div>\
+                <div style="float:right;"><button id="rightButton" onclick="clickRight(event)" class="'+buttonRightClass+'"><\/button> <\/div>\
+            <\/div>\
         <\/div>\
     <\/div>');
     
@@ -24,7 +28,7 @@ function initialNavigation(containerSelector,specialComponentSelector,background
         $(e).addClass("testNavDiv");
         $(e).css("top","0px");
         $(e).css("position","absolute");
-        $(e).css("width","100%");
+        $(e).css("width",966+"px");
         $(e).css("left", i * 966 + "px");
         count++;
     });
