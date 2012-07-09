91751e314c57a0bacb506113632c2270e523fb49
MatSam <MadManMathew@gmail.com>
2012-07-09 14:36:20 +1000

    remove blog.js alerts

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -1,8 +1,6 @@
 $("code").each(function(i,e){
   var codeHtml = $(e).html();
-    alert(codeHtml);
   codeHtml = codeHtml.replace(/</g,'&lt;');
   codeHtml = codeHtml.replace(/>/g,'&gt;');
   $(e).html(codeHtml);
-    alert(codeHtml);
 });
