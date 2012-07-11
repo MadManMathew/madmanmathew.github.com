d61709962cedf929cab785ac09867aab33562de0
MatSam <MadManMathew@gmail.com>
2012-07-11 15:21:35 +1000

    change blogjs again

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -1,7 +1,7 @@
 $("code").each(function(i,e){
   var codeHtml = $(e).html();
-  codeHtml = codeHtml.replace(/([^\\])</g,'$1&lt;');
-  codeHtml = codeHtml.replace(/([^\\]>/g,'$1&gt;');
-  codeHtml = codeHtml.replace(/[\\]([\S]+)[\\]/g,'<$1>');
+  codeHtml = codeHtml.replace(/</g,'&lt;');
+  codeHtml = codeHtml.replace(/>/g,'&gt;');
+  codeHtml = codeHtml.replace(/[\\]{1}([\S]+)[\\]{1}/g,'<$1>');
   $(e).html(codeHtml);
 });
